a34babf59971e2d8470299a50976b6038fdbe53d
Noah Tuchow <nwtuchow@gmail.com>
2024-06-10 13:16:32 -0400

    fixed problem in contrast calculation
    Now both use S defined the same way as L/a^2 instead of L/d^2 where d is the true separation, since S is computed at an earlier step with semimajor axis, and can't be easily changed.

--- bioverse/functions.py
+++ bioverse/functions.py
@@ -1101,10 +1101,10 @@ def compute_contrast(d,at_quadrature=True,phasefunc=lambertian_phase):
         d['contrast'] = d['A_g'] * (4.258756e-5 * d['R'] / d['a'])**2 / np.pi
     else:
         try:
-            #contrast at given phase, requires knowledge of phase angle, true_sep
-            d['contrast']= d['A_g'] * pow((4.258756e-5 * d['R'] / d['true_sep']),2)*phasefunc(d['phase_angle'])
+            #contrast at given phase, requires knowledge of phase angle
+            d['contrast']= d['A_g'] * pow((4.258756e-5 * d['R'] / d['a']),2)*phasefunc(d['phase_angle'])
         except KeyError:
-            raise KeyError("Missing keys for 'true_sep','phase_angle'")
+            raise KeyError("Missing keys for 'A_g', 'R','a', or 'phase_angle'")
 
     return d
 
